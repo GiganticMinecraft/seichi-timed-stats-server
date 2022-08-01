89c2e4ff967c29ad2933f9061d20119dd64a588a
Ryosuke <korygm33@gmail.com>
2022-08-02 02:32:12 +0900

    feat: add two minimal modules

---
+++ timed-stats-translator/build.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use std::process::{exit, Command, ExitStatus};
+
+// From
+// https://github.com/neoeinstein/protoc-gen-prost/blob/fe8e21a9d319c305cda0cfddd146ccddc73d36dd/example/build-with-buf/build.rs
+
+fn process_status(status: ExitStatus) {
+    if !status.success() {
+        exit(status.code().unwrap_or(-1))
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    process_status(
+        Command::new("buf")
+            .arg("generate")
+            .arg("buf.build/gigantic-minecraft/seichi-game-data")
+            .current_dir(env!("CARGO_MANIFEST_DIR"))
+            .status()
+            .unwrap(),
+    );
+
+    Ok(())
+}
