ad22a1fae4585f2d80805d702f97610b3be79fb0
Ryosuke <korygm33@gmail.com>
2022-08-04 00:11:39 +0900

    feat: implement repository

--- servers/translator/src/main.rs
+++ servers/translator/src/main.rs
@@ -2,8 +2,6 @@
 #![warn(clippy::nursery, clippy::pedantic)]
 #![allow(clippy::cargo_common_metadata)]
 
-use std::sync::Arc;
-
 pub(crate) mod domain {
     #[derive(Debug, Clone)]
     pub struct Player {
@@ -13,25 +11,25 @@ pub(crate) mod domain {
 
     #[derive(Debug, Clone)]
     pub struct PlayerBreakCount {
-        pub player: Option<Player>,
+        pub player: Player,
         pub break_count: u64,
     }
 
     #[derive(Debug, Clone)]
     pub struct PlayerBuildCount {
-        pub player: Option<Player>,
+        pub player: Player,
         pub build_count: u64,
     }
 
     #[derive(Debug, Clone)]
     pub struct PlayerPlayTicks {
-        pub player: Option<Player>,
+        pub player: Player,
         pub play_ticks: u64,
     }
 
     #[derive(Debug, Clone)]
     pub struct PlayerVoteCount {
-        pub player: Option<Player>,
+        pub player: Player,
         pub vote_count: u64,
     }
 
@@ -110,66 +108,202 @@ pub(crate) mod infra_axum_handlers {
 }
 
 pub(crate) mod infra_repository_impls {
-    use crate::domain::{
-        PlayerBreakCount, PlayerBuildCount, PlayerDataRepository, PlayerPlayTicks, PlayerVoteCount,
-    };
-
     #[allow(dead_code)]
     #[allow(clippy::nursery, clippy::pedantic)]
-    pub(crate) mod buf_generated {
+    mod buf_generated {
         include!("gen/mod.rs");
     }
 
-    pub struct SeichiGameDataGrpcClient {}
+    pub mod config {
+        #[derive(serde::Deserialize, Debug, Clone)]
+        pub struct GrpcClientConfig {
+            pub game_data_server_grpc_endpoint_url: String,
+        }
+
+        impl GrpcClientConfig {
+            pub fn from_env() -> anyhow::Result<GrpcClientConfig> {
+                Ok(envy::from_env::<GrpcClientConfig>()?)
+            }
+        }
+    }
+
+    mod buf_generated_to_domain {
+        use super::buf_generated::gigantic_minecraft::seichi_game_data::v1 as generated;
+        use crate::domain;
+
+        fn into_domain_player(p: generated::Player) -> domain::Player {
+            domain::Player {
+                uuid: p.uuid,
+                last_known_name: p.last_known_name,
+            }
+        }
+
+        fn extract_domain_player(
+            player: Option<generated::Player>,
+        ) -> anyhow::Result<domain::Player> {
+            let player = player.ok_or_else(|| anyhow::anyhow!("Player field not set"))?;
+
+            Ok(into_domain_player(player))
+        }
+
+        pub fn try_into_domain_player_break_count(
+            value: generated::PlayerBreakCount,
+        ) -> anyhow::Result<domain::PlayerBreakCount> {
+            Ok(domain::PlayerBreakCount {
+                player: extract_domain_player(value.player)?,
+                break_count: value.break_count,
+            })
+        }
+
+        pub fn try_into_domain_player_build_count(
+            value: generated::PlayerBuildCount,
+        ) -> anyhow::Result<domain::PlayerBuildCount> {
+            Ok(domain::PlayerBuildCount {
+                player: extract_domain_player(value.player)?,
+                build_count: value.build_count,
+            })
+        }
+
+        pub fn try_into_domain_player_play_ticks(
+            value: generated::PlayerPlayTicks,
+        ) -> anyhow::Result<domain::PlayerPlayTicks> {
+            Ok(domain::PlayerPlayTicks {
+                player: extract_domain_player(value.player)?,
+                play_ticks: value.play_ticks,
+            })
+        }
+
+        pub fn try_into_domain_player_vote_count(
+            value: generated::PlayerVoteCount,
+        ) -> anyhow::Result<domain::PlayerVoteCount> {
+            Ok(domain::PlayerVoteCount {
+                player: extract_domain_player(value.player)?,
+                vote_count: value.vote_count,
+            })
+        }
+    }
+
+    use buf_generated::gigantic_minecraft::seichi_game_data::v1::read_service_client::ReadServiceClient;
+    type GameDataGrpcClient = ReadServiceClient<tonic::transport::Channel>;
+
+    pub struct SeichiGameDataGrpcClient {
+        client: GameDataGrpcClient,
+    }
+
+    impl SeichiGameDataGrpcClient {
+        pub async fn try_connect_with(
+            config: config::GrpcClientConfig,
+        ) -> anyhow::Result<SeichiGameDataGrpcClient> {
+            let client =
+                GameDataGrpcClient::connect(config.game_data_server_grpc_endpoint_url).await?;
+
+            Ok(SeichiGameDataGrpcClient { client })
+        }
+
+        pub(crate) fn cloned_inner(&self) -> GameDataGrpcClient {
+            self.client.clone()
+        }
+    }
+
+    fn empty_request() -> tonic::Request<pbjson_types::Empty> {
+        tonic::Request::new(pbjson_types::Empty::default())
+    }
+
+    use crate::domain::{PlayerBreakCount, PlayerBuildCount, PlayerPlayTicks, PlayerVoteCount};
 
     #[async_trait::async_trait]
-    impl PlayerDataRepository for SeichiGameDataGrpcClient {
+    impl crate::domain::PlayerDataRepository for SeichiGameDataGrpcClient {
         async fn get_all_break_counts(&self) -> anyhow::Result<Vec<PlayerBreakCount>> {
-            // TODO: Call API
-            Ok(vec![])
+            Ok(self
+                .cloned_inner()
+                .break_counts(empty_request())
+                .await?
+                .into_inner()
+                .results
+                .into_iter()
+                .map(buf_generated_to_domain::try_into_domain_player_break_count)
+                .collect::<Result<_, _>>()?)
         }
 
         async fn get_all_build_counts(&self) -> anyhow::Result<Vec<PlayerBuildCount>> {
-            // TODO: Call API
-            Ok(vec![])
+            Ok(self
+                .cloned_inner()
+                .build_counts(empty_request())
+                .await?
+                .into_inner()
+                .results
+                .into_iter()
+                .map(buf_generated_to_domain::try_into_domain_player_build_count)
+                .collect::<Result<_, _>>()?)
         }
 
         async fn get_all_play_ticks(&self) -> anyhow::Result<Vec<PlayerPlayTicks>> {
-            // TODO: Call API
-            Ok(vec![])
+            Ok(self
+                .cloned_inner()
+                .play_ticks(empty_request())
+                .await?
+                .into_inner()
+                .results
+                .into_iter()
+                .map(buf_generated_to_domain::try_into_domain_player_play_ticks)
+                .collect::<Result<_, _>>()?)
         }
 
         async fn get_all_vote_counts(&self) -> anyhow::Result<Vec<PlayerVoteCount>> {
-            // TODO: Call API
-            Ok(vec![])
+            Ok(self
+                .cloned_inner()
+                .vote_counts(empty_request())
+                .await?
+                .into_inner()
+                .results
+                .into_iter()
+                .map(buf_generated_to_domain::try_into_domain_player_vote_count)
+                .collect::<Result<_, _>>()?)
         }
+    }
+}
+
+pub(crate) mod app {
+    use crate::infra_axum_handlers;
+    use crate::infra_repository_impls;
+    use std::sync::Arc;
+
+    pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+        // initialize tracing
+        tracing_subscriber::fmt::init();
+
+        let repository = {
+            let client_config = infra_repository_impls::config::GrpcClientConfig::from_env()?;
+            let client =
+                infra_repository_impls::SeichiGameDataGrpcClient::try_connect_with(client_config)
+                    .await?;
+
+            Arc::new(client)
+        };
+
+        let app = {
+            use infra_axum_handlers::handle_get_metrics;
+
+            use axum::routing::get;
+            use axum::Router;
+
+            Router::new().route("/metrics", get(handle_get_metrics(&repository)))
+        };
+
+        let addr = {
+            use std::net::SocketAddr;
+            SocketAddr::from(([0, 0, 0, 0], 80))
+        };
+
+        tracing::debug!("listening on {}", addr);
+
+        Ok(axum::Server::bind(&addr)
+            .serve(app.into_make_service())
+            .await?)
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-
-    let repository = Arc::new(infra_repository_impls::SeichiGameDataGrpcClient {});
-
-    let app = {
-        use infra_axum_handlers::handle_get_metrics;
-
-        use axum::routing::get;
-        use axum::Router;
-
-        Router::new().route("/metrics", get(handle_get_metrics(&repository)))
-    };
-
-    let addr = {
-        use std::net::SocketAddr;
-        SocketAddr::from(([0, 0, 0, 0], 80))
-    };
-
-    tracing::debug!("listening on {}", addr);
-
-    Ok(axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await?)
+    app::main().await
 }
